4f2aaa27635f5a2b1b22ebf46e47635cce447356
adplincinst <apadilla@lincolninst.edu>
2024-07-24 00:29:53 +0000

    fixed script

--- scripts/iow/start-gleaner.sh
+++ scripts/iow/start-gleaner.sh
@@ -19,16 +19,16 @@ if [  -z "$1 ]
 then
   echo "Usage: $0 GLEANERCONFIG_PATH" && exit 1
 fi
-
+GLEANERCONFIG_PATH="$1"
 
 for src in `cat $GLEANERCONFIG_PATH | grep '\Wname:'|awk '{print $2}'`
 do
 
-OUTFILE="$LOGDIR/gleaner-$src.out"
-ERRFILE="$LOGDIR/gleaner-$src.err"
+	OUTFILE="$LOGDIR/gleaner-$src.out"
+	ERRFILE="$LOGDIR/gleaner-$src.err"
 
-echo "harvesting source '$src'..."
-bin/gleaner -log debug  -cfg $GLEANERCONFIG_PATH -source $src -rude # > $OUTFILE 2>$ERRFILE
+	echo "harvesting source '$src'..."
+	bin/gleaner -log debug  -cfg $GLEANERCONFIG_PATH -source $src -rude # > $OUTFILE 2>$ERRFILE
 done
 echo "complete!"
 
