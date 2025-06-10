d50459427488913456d0b4fbc217c752f4c9a68c
Colton Loftus <70598503+C-Loftus@users.noreply.github.com>
2025-06-10 09:11:50 -0400

    cargo workspaces

---
+++ shacl_validator/validator/src/wasm.rs
@@ -0,0 +1,20 @@
+// Copyright 2025 Lincoln Institute of Land Policy
+// SPDX-License-Identifier: Apache-2.0
+
+use shacl_validation::store::ShaclDataManager;
+use srdf::{RDFFormat};
+use wasm_bindgen::prelude::*;
+use std::io::Cursor;
+
+
+#[wasm_bindgen]
+pub fn validate_triples_with_raw_shacl(shacl: &str, triples: &str) -> Result<String, String> {
+    match ShaclDataManager::load(Cursor::new(shacl), RDFFormat::Turtle, None) {
+        Ok(schema) => match validation::validate_triples(&schema, triples) {
+            Ok(report) => Ok(report.to_string()),
+            Err(err) => Err(err.to_string()),
+        },
+        Err(err) => Err(err.to_string()),
+    }
+}
+
