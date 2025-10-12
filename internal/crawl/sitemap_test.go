04021d78d891c962fb5f4f984615c1f4df7a1e79
Colton Loftus <70598503+C-Loftus@users.noreply.github.com>
2025-10-12 18:14:19 -0400

    more tests

--- internal/crawl/sitemap_test.go
+++ internal/crawl/sitemap_test.go
@@ -7,15 +7,75 @@ import (
 	"bytes"
 	"context"
 	"errors"
+	"io"
+	"os"
+	"strings"
 	"testing"
 	"time"
 
 	common "github.com/internetofwater/nabu/internal/common"
 	"github.com/internetofwater/nabu/internal/crawl/storage"
+	"github.com/internetofwater/nabu/internal/crawl/url_info"
 	"golang.org/x/sync/errgroup"
 
 	"github.com/stretchr/testify/require"
 )
+
+func TestNamesAreBase64(t *testing.T) {
+	mocks := map[string]common.MockResponse{
+		"https://geoconnex.us/sitemap/iow/wqp/stations__5.xml": {
+			StatusCode: 200,
+			File:       "testdata/sitemap.xml",
+		},
+		"https://geoconnex.us/iow/wqp/BPMWQX-1084-WR-CC01C": {
+			StatusCode:  200,
+			File:        "testdata/reference_feature.jsonld",
+			ContentType: "application/ld+json",
+		},
+		"https://geoconnex.us/iow/wqp/BPMWQX-1085-WR-CC01C2": {
+			StatusCode:  200,
+			File:        "testdata/reference_feature_2.jsonld",
+			ContentType: "application/ld+json",
+		},
+		"https://geoconnex.us/iow/wqp/BPMWQX-1086-WR-CC02A": {
+			StatusCode:  200,
+			File:        "testdata/reference_feature_3.jsonld",
+			ContentType: "application/ld+json",
+		},
+		"https://geoconnex.us/robots.txt": {
+			StatusCode:  200,
+			File:        "testdata/geoconnex_robots.txt",
+			ContentType: "application/text/plain",
+		},
+	}
+	mockedClient := common.NewMockedClient(
+		true, mocks,
+	)
+
+	storage, err := storage.NewLocalTempFSCrawlStorage()
+	require.NoError(t, err)
+	sitemap, err := NewSitemap(context.Background(), mockedClient, "https://geoconnex.us/sitemap/iow/wqp/stations__5.xml", 1, storage, "test")
+	require.NoError(t, err)
+	config, err := NewSitemapHarvestConfig(mockedClient, sitemap, "", false, false)
+	require.NoError(t, err)
+
+	_, _, err = sitemap.
+		Harvest(context.Background(), &config)
+	require.NoError(t, err)
+
+	const root = ""
+	storageItems, err := storage.ListDir(root)
+	require.NoError(t, err)
+	require.Equal(t, len(storageItems), 2, "There are 3 mocks so there is a list of length 2. Thus there should also be a storage items of list length 2")
+	for item := range mocks {
+		if strings.HasSuffix(item, ".jsonld") {
+			url := url_info.NewUrlFromString(item)
+			path, err := urlToStoragePath(sitemap.sitemapId, url)
+			require.NoError(t, err)
+			require.Contains(t, storageItems, path)
+		}
+	}
+}
 
 func TestHarvestSitemap(t *testing.T) {
 
@@ -59,6 +119,97 @@ func TestHarvestSitemap(t *testing.T) {
 	require.NoError(t, err)
 
 	require.Equal(t, results.SuccessfulSites, 3)
+}
+
+func TestHarvestTwiceOverridesFile(t *testing.T) {
+
+	const urlToHarvestDifferently = "https://geoconnex.us/iow/wqp/BPMWQX-1084-WR-CC01C"
+	mocks := map[string]common.MockResponse{
+		"https://geoconnex.us/sitemap/iow/wqp/stations__5.xml": {
+			StatusCode: 200,
+			File:       "testdata/sitemap.xml",
+		},
+		urlToHarvestDifferently: {
+			StatusCode:  200,
+			File:        "testdata/reference_feature.jsonld",
+			ContentType: "application/ld+json",
+		},
+		"https://geoconnex.us/iow/wqp/BPMWQX-1085-WR-CC01C2": {
+			StatusCode:  200,
+			File:        "testdata/reference_feature_2.jsonld",
+			ContentType: "application/ld+json",
+		},
+		"https://geoconnex.us/iow/wqp/BPMWQX-1086-WR-CC02A": {
+			StatusCode:  200,
+			File:        "testdata/reference_feature_3.jsonld",
+			ContentType: "application/ld+json",
+		},
+		"https://geoconnex.us/robots.txt": {
+			StatusCode:  200,
+			File:        "testdata/geoconnex_robots.txt",
+			ContentType: "application/text/plain",
+		},
+	}
+	mockedClient := common.NewMockedClient(
+		true,
+		mocks,
+	)
+
+	storage, err := storage.NewLocalTempFSCrawlStorage()
+	require.NoError(t, err)
+	sitemap, err := NewSitemap(context.Background(), mockedClient, "https://geoconnex.us/sitemap/iow/wqp/stations__5.xml", 1, storage, "test")
+	require.NoError(t, err)
+	config, err := NewSitemapHarvestConfig(mockedClient, sitemap, "", false, false)
+	require.NoError(t, err)
+
+	_, _, err = sitemap.
+		Harvest(context.Background(), &config)
+	require.NoError(t, err)
+
+	pathInStorage, err := urlToStoragePath(sitemap.sitemapId, url_info.NewUrlFromString(urlToHarvestDifferently))
+	require.NoError(t, err)
+	dataInStorage, err := storage.Get(pathInStorage)
+	require.NoError(t, err)
+	dataInStorageAsBytes, err := io.ReadAll(dataInStorage)
+	require.NoError(t, err)
+
+	mockedContent, err := os.Open("testdata/reference_feature.jsonld")
+	require.NoError(t, err)
+	mockedContentAsBytes, err := io.ReadAll(mockedContent)
+	require.NoError(t, err)
+	require.Equal(t, dataInStorageAsBytes, mockedContentAsBytes)
+
+	/*
+		Once we have assured that the file is in the storage, we try
+		harvesting it again but this time with a different content
+		to make sure the the file is overwritten
+	*/
+
+	mocks[urlToHarvestDifferently] = common.MockResponse{
+		StatusCode: 200,
+		// new content
+		File:        "testdata/reference_feature_2.jsonld",
+		ContentType: "application/ld+json",
+	}
+	mockClientWithReplacedContent := common.NewMockedClient(true, mocks)
+	sitemap, err = NewSitemap(context.Background(), mockClientWithReplacedContent, "https://geoconnex.us/sitemap/iow/wqp/stations__5.xml", 1, storage, "test")
+	require.NoError(t, err)
+	stats, _, err := sitemap.
+		Harvest(context.Background(), &config)
+	require.NoError(t, err)
+
+	require.Equal(t, stats.SuccessfulSites, 3)
+	require.Len(t, stats.CrawlFailures, 0, "If we harvest the same content again and there is no bad status code, there should be no failures")
+
+	dataInStorage, err = storage.Get(pathInStorage)
+	require.NoError(t, err)
+	dataInStorageAsBytes, err = io.ReadAll(dataInStorage)
+	require.NoError(t, err)
+	mockedContent, err = os.Open("testdata/reference_feature_2.jsonld")
+	require.NoError(t, err)
+	mockedContentAsBytes, err = io.ReadAll(mockedContent)
+	require.NoError(t, err)
+	require.Equal(t, dataInStorageAsBytes, mockedContentAsBytes, "The file should have been overwritten and contain the new content")
 }
 
 func TestHarvestSitemapWithCleanup(t *testing.T) {
@@ -109,12 +260,16 @@ func TestHarvestSitemapWithCleanup(t *testing.T) {
 	err = storage.StoreWithoutServersideHash("summoned/"+sitemap.sitemapId+"/testfile3.txt", bytes.NewReader([]byte("dummy_data")))
 	require.NoError(t, err)
 
-	_, cleanupChan, err := sitemap.
+	stats, cleanupChan, err := sitemap.
 		Harvest(context.Background(), &config)
 	require.NoError(t, err)
 	// wait for cleanup to finish
 	cleanedUpFiles := <-cleanupChan
 	require.Len(t, cleanedUpFiles, 3, "THere should be 3 files cleaned up, representing the 3 testfiles.txt that were manually added")
+
+	require.Len(t, stats.CrawlFailures, 0)
+	require.Equal(t, stats.SuccessfulSites, 3)
+	require.Len(t, stats.WarningStats.ShaclWarnings, 0)
 
 	mockedClientWithErrors := common.NewMockedClient(
 		true,
@@ -155,12 +310,17 @@ func TestHarvestSitemapWithCleanup(t *testing.T) {
 	config, err = NewSitemapHarvestConfig(mockedClientWithErrors, sitemap, "", false, true)
 	require.NoError(t, err)
 
-	_, cleanupChan, err = sitemap.
+	stats, cleanupChan, err = sitemap.
 		Harvest(context.Background(), &config)
 	require.NoError(t, err)
 	// wait for cleanup to finish
 	cleanedUpFiles = <-cleanupChan
 	require.Len(t, cleanedUpFiles, 1, "There should be 1 file cleaned up, representing dummy.txt; the other sites which 404d but are in the sitemap should stay in case they are temporarily failing")
+
+	require.Len(t, stats.CrawlFailures, 2, "Two sites had 404 errors")
+	require.Equal(t, stats.SuccessfulSites, 1, "Only one site had a successful response")
+	require.Len(t, stats.WarningStats.ShaclWarnings, 0)
+	require.Equal(t, 3, stats.SitesInSitemap, "All 3 sites should be in the sitemap")
 }
 
 func TestErrorGroupCtxCancelling(t *testing.T) {
