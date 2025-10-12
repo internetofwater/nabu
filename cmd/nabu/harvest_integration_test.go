e34a1b1d8f45efd4f90427c1c4cdcf4736c7cd01
Colton Loftus <70598503+C-Loftus@users.noreply.github.com>
2025-10-12 14:25:57 -0400

    tests passing

--- cmd/nabu/harvest_integration_test.go
+++ cmd/nabu/harvest_integration_test.go
@@ -5,6 +5,7 @@ package main
 
 import (
 	"context"
+	"encoding/base64"
 	"fmt"
 	"testing"
 	"time"
@@ -59,7 +60,10 @@ func (s *GleanerInterationSuite) TestIntegrationWithNabu() {
 	err = client.SyncTriplestoreGraphs(ctx, "summoned/", true)
 	s.Require().NoError(err)
 
-	exists, err := client.GraphClient.GraphExists(context.Background(), "urn:iow:summoned:iow_wqp_stations__5:1c9ccd5f69ee83108a2f849c8c3afcc0f255e2d1a7c56dfe55b3728ef357c5fe.jsonld")
+	const pid = "https://geoconnex.us/iow/wqp/BPMWQX-1084-WR-CC01C"
+	encodedPid := base64.StdEncoding.EncodeToString([]byte(pid))
+	s.Require().Equal("aHR0cHM6Ly9nZW9jb25uZXgudXMvaW93L3dxcC9CUE1XUVgtMTA4NC1XUi1DQzAxQw==", encodedPid)
+	exists, err := client.GraphClient.GraphExists(context.Background(), "urn:iow:summoned:iow_wqp_stations__5:"+encodedPid+".jsonld")
 	s.Require().NoError(err)
 	s.Require().True(exists)
 }
