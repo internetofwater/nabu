b317e4dc64ce7dee7939ec9f76a38b795ddbb9b1
Colton Loftus <70598503+C-Loftus@users.noreply.github.com>
2025-10-13 08:53:49 -0400

    backup

--- cmd/nabu/test.go
+++ cmd/nabu/test.go
@@ -4,14 +4,16 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"crypto/md5"
 	"fmt"
 
 	"github.com/internetofwater/nabu/internal/synchronizer"
-
 	log "github.com/sirupsen/logrus"
 )
 
+// Ensure that an S3 bucket is compatible with what we need for Nabu
 func Test(ctx context.Context, client *synchronizer.SynchronizerClient) error {
 	exists, err := client.S3Client.Client.BucketExists(ctx, client.S3Client.DefaultBucket)
 	if err != nil {
@@ -22,7 +24,22 @@ func Test(ctx context.Context, client *synchronizer.SynchronizerClient) error {
 		return fmt.Errorf("default bucket %s does not exist", client.S3Client.DefaultBucket)
 	}
 
-	log.Info("s3 test passed")
+	testData := []byte("test data")
+	if err := client.S3Client.StoreWithHash("test", bytes.NewReader(testData), len(testData)); err != nil {
+		return err
+	}
+
+	md5Hash, err := client.S3Client.GetHash("test")
+	if err != nil {
+		return fmt.Errorf("failed to get hash: %w", err)
+	}
+
+	calculatedHash := md5.Sum(testData)
+	if md5Hash != fmt.Sprintf("%x", calculatedHash) {
+		return fmt.Errorf("hashes do not match")
+	}
+
+	log.Info("S3 test passed; you should be able to use this bucket with Nabu")
 
 	return nil
 }
