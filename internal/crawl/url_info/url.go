8833f3608cc47d45fd66725f8930e124188b974d
Colton Loftus <70598503+C-Loftus@users.noreply.github.com>
2025-10-15 08:58:10 -0400

    Fix sitemap parse nil pointer deref (#247)

--- internal/crawl/url_info/url.go
+++ internal/crawl/url_info/url.go
@@ -12,23 +12,32 @@ import (
 
 // Represents a URL tag and its attributes within a sitemap
 type URL struct {
+	// the url link itself
 	Loc        string  `xml:"loc"`
 	LastMod    string  `xml:"lastmod"`
 	ChangeFreq string  `xml:"changefreq"`
 	Priority   float32 `xml:"priority"`
 
-	// A base64 encoded version of the loc that can be used as a key for lookups
+	// A base64 encoded version of the url link loc that can be used as a key for lookups
 	Base64Loc string `xml:"-"`
 }
 
 func NewUrlFromSitemapEntry(sitemap sitemap.Entry) *URL {
+
+	// need to check this to prevent a nil pointer deref panic
+	var lastModified string
+	if sitemap.GetLastModified() != nil {
+		lastModified = sitemap.GetLastModified().String()
+	} else {
+		lastModified = ""
+	}
 
 	return &URL{
 		// trim space just in case the creator of the sitemap
 		// messed up and added spaces at the start of the url
 		// preventing it from being fetched properly
 		Loc:        strings.TrimSpace(sitemap.GetLocation()),
-		LastMod:    sitemap.GetLastModified().String(),
+		LastMod:    lastModified,
 		ChangeFreq: sitemap.GetChangeFrequency(),
 		Priority:   sitemap.GetPriority(),
 		Base64Loc:  base64.StdEncoding.EncodeToString([]byte(sitemap.GetLocation())),
