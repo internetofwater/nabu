9fbcb566df2a1de17c4660f95bee01e28d6121bb
Colton Loftus <70598503+C-Loftus@users.noreply.github.com>
2026-06-10 08:01:23 -0400

    Push latest version of triples to geoparquet with error handling code

--- triples_to_geoparquet/src/parquet_lib.rs
+++ triples_to_geoparquet/src/parquet_lib.rs
@@ -3,10 +3,10 @@
 
 use geoarrow_schema::WkbType;
 
-use arrow_schema::{DataType::Utf8, Field, Schema, SchemaBuilder};
+use arrow_schema::{DataType::{LargeUtf8, Utf8}, Field, Schema, SchemaBuilder};
 use geoarrow_schema::GeoArrowType;
 use geoparquet::writer::{GeoParquetRecordBatchEncoder, GeoParquetWriterOptionsBuilder};
-use parquet::arrow::ArrowWriter;
+use parquet::{arrow::ArrowWriter};
 
 const GEOMETRY_COLUMN_NAME: &str = "geometry";
 
@@ -37,17 +37,17 @@ pub fn generate_schema() -> Schema {
     let geometry_field = geoarrow_type.to_field(GEOMETRY_COLUMN_NAME, false);
     schema_builder.push(geometry_field);
 
-    let geoconnex_pid = Field::new("id", Utf8, false);
-    schema_builder.push(geoconnex_pid);
+    // let geoconnex_pid = Field::new("id", Utf8, false);
+    // schema_builder.push(geoconnex_pid);
 
-    let geoconnex_sitemap = Field::new("geoconnex_sitemap", Utf8, false);
-    schema_builder.push(geoconnex_sitemap);
+    // let geoconnex_sitemap = Field::new("geoconnex_sitemap", Utf8, false);
+    // schema_builder.push(geoconnex_sitemap);
 
-    let feature_name = Field::new("feature_name", Utf8, true);
-    schema_builder.push(feature_name);
+    // let feature_name = Field::new("feature_name", LargeUtf8, true);
+    // schema_builder.push(feature_name);
 
-    let feature_description = Field::new("feature_description", Utf8, true);
-    schema_builder.push(feature_description);
+    // let feature_description = Field::new("feature_description", LargeUtf8, true);
+    // schema_builder.push(feature_description);
 
     schema_builder.finish()
 }
